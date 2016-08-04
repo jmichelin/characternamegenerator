5df1b4d72f3fdd5f3d005ead294fdd4285861da4
jmichelin <jmichelinphone@gmail.com>
2016-08-04 09:21:06 -0400

    [chore] adds testing framework

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jmichelin on 8/3/16.
+ */
+var expect = require('chai').expect;
+var randomNameGenerator = require('./index');
+
+describe('tests should be running', function () {
+    it('should work', function(){
+        expect(true).to.be.true;
+    });
+});
